21361ae67e00bceb4f77ad05b26203d4876ce4fe
Joshua Chelashaw <joshua.chelashaw@students.uts.edu.au>
2026-03-06 20:59:07 +0800

    Initial Commit

---
+++ log/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  log
+//
+//  Created by joshua chelashaw on 6/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ log/logApp.swift
@@ -0,0 +1,17 @@
+//
+//  logApp.swift
+//  log
+//
+//  Created by joshua chelashaw on 6/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
